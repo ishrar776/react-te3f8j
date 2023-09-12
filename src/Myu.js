fc267bc911cba2b00c568c80f497444f99bdf511
ishrar776 <89441607+ishrar776@users.noreply.github.com>
2023-09-12 12:43:52 +0530

    1 file modified

--- src/Myu.js
+++ src/Myu.js
@@ -2,10 +2,11 @@ import React, { useState, useContext, useEffect } from 'react';
 import { GlobalData } from './Context';
 const Myu = () => {
   const { message, myId } = useContext(GlobalData);
+  const displayMsg = myId.updateMyId;
   return (
     <div>
       I am Myu component
-      {myId.updateMyId}
+      {displayMsg}
     </div>
   );
 };
